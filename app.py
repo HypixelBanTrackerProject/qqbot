3c1cf8da0021e88956b3bc2ffd6549b2f0dc49e7
SakuraNiroku <cherryhikari@outlook.com>
2025-05-01 14:10:06 +0800

    fix(appsettings.json)

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import subprocess
 from alicebot import Bot
+import shutil
 
 LAGRANGE = './lagrange/lagrange'
 WEBAPI = 'fastapi run'
+
+shutil.copy('lagrange/appsettings.json','lagrange/config/appsettings.json')
 
 subprocess.Popen(LAGRANGE,cwd='lagrange/config')
 subprocess.Popen(WEBAPI,cwd='webapi')
